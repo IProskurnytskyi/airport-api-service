af31b90cb8eb32bd57830c02147fea6ec0879e2e
Ivan Proskurnytskyi <prsky.ivan@gmail.com>
2023-10-31 17:33:00 +0100

    Added views and routs

--- airport/urls.py
+++ airport/urls.py
@@ -1,8 +1,27 @@
 from django.urls import path, include
 from rest_framework import routers
 
+from airport.views import (
+    AirplaneTypeViewSet,
+    AirplaneViewSet,
+    CrewViewSet,
+    AirportViewSet,
+    RouteViewSet,
+    FlightViewSet,
+    OrderViewSet,
+    TicketViewSet
+)
 
 router = routers.DefaultRouter()
+router.register("airplane_types", AirplaneTypeViewSet)
+router.register("airplanes", AirplaneViewSet)
+router.register("crew", CrewViewSet)
+router.register("airports", AirportViewSet)
+router.register("routs", RouteViewSet)
+router.register("flights", FlightViewSet)
+router.register("orders", OrderViewSet)
+router.register("tickets", TicketViewSet)
+
 
 urlpatterns = [path("", include(router.urls))]
 
