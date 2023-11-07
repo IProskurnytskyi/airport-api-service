1312abd7a671bb9edbcf84abc655e8b97a2cf626
Ivan Proskurnytskyi <prsky.ivan@gmail.com>
2023-11-07 12:32:45 +0100

    Added related name to Order model

--- airport/models.py
+++ airport/models.py
@@ -112,7 +112,9 @@ class Flight(models.Model):
 class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="orders"
     )
 
     def __str__(self) -> str:
